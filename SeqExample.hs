1843724fccb31efec7207beb6b08fe81416cbb5d
Wesley Tanaka <wtanaka@yahoo.com>
2014-09-26 16:09:01 -1000

    Data.Sequence example

---
+++ SeqExample.hs
@@ -0,0 +1,37 @@
+#!/usr/bin/env runhaskell
+--
+-- Copyright 2014 Wesley Tanaka <http://wtanaka.com/>
+--
+-- This file is part of https://github.com/wtanaka/haskell
+--
+-- https://github.com/wtanaka/haskell is free software: you can
+-- redistribute it and/or modify it under the terms of the GNU General
+-- Public License as published by the Free Software Foundation,
+-- either version 3 of the License, or (at your option) any later
+-- version.
+--
+-- https://github.com/wtanaka/haskell is distributed in the hope that
+-- it will be useful, but WITHOUT ANY WARRANTY; without even the
+-- implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+-- PURPOSE.  See the GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with https://github.com/wtanaka/haskell .  If not, see
+-- <http://www.gnu.org/licenses/>.
+
+module Main(main) where
+
+import Data.Sequence
+
+-- fromList [3,1,2,4]
+test1 :: Seq Int
+test1 = let
+   a = (empty :: Seq Int)
+   b = 1 <| a
+   c = b |> 2
+   d = 3 <| c
+   e = d |> 4
+   in e
+
+main :: IO ()
+main = print test1
